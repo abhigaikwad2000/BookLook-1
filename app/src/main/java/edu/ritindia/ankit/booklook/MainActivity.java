8b6365fe999a7692df7bb93467c66c7f94f73c07
abhishek gaikwad <abhigaikwad174@gmail.com>
2021-05-13 23:20:05 +0530

    commit 1

---
+++ app/src/main/java/edu/ritindia/ankit/booklook/MainActivity.java
@@ -0,0 +1,34 @@
+package edu.ritindia.ankit.booklook;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setTitle("Home");
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#4fc3f7"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+/////hii ankit testing github integration with anderoid stdio
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menus,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
